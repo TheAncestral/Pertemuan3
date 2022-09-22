1812cf37bebe00273b3e0a9ef2ee579dcb75e794
TheAncestral <113588493+TheAncestral@users.noreply.github.com>
2022-09-22 15:36:31 +0700

    Buat matakuliah.java

---
+++ matakuliah.java
@@ -0,0 +1,6 @@
+class matakuliah {
+  System.out.println("Mata kuliah yang ada di jurusan Informatika : ");
+  System.out.println("1. Algoritma dan Pemograman");
+  System.out.println("2. Teknologi Cloud");
+  System.out.println("3. Jaringan Komputer");
+}
