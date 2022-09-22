e331b3a045b2b7705c572b73b5ac986522131447
TheAncestral <113588493+TheAncestral@users.noreply.github.com>
2022-09-22 15:57:26 +0700

    Buat ukm.java

---
+++ ukm.java
@@ -0,0 +1,6 @@
+class ukm {
+  System.out.println("UKM yang ada di UTDI : ");
+  System.out.println("1. UKM Futsal");
+  System.out.println("2. UKM Informatika");
+  System.out.println("3. UKM Kesenian");
+}
