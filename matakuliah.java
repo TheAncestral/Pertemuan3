00d85d00a703cbf569cd2137345b88e364a89714
TheAncestral <113588493+TheAncestral@users.noreply.github.com>
2022-09-22 15:45:57 +0700

    Tambah mata kuliah dan memindah posisi mata kuliah
    Tambah Matematika
    Pindah Jaringan Komputer

--- matakuliah.java
+++ matakuliah.java
@@ -2,5 +2,6 @@ class matakuliah {
   System.out.println("Mata kuliah yang ada di jurusan Informatika : ");
   System.out.println("1. Algoritma dan Pemograman");
   System.out.println("2. Teknologi Cloud");
-  System.out.println("3. Jaringan Komputer");
+  System.out.println("3. Matematika");
+  System.out.println("4. Jaringan Komputer");
 }
